a916480224196bf2b3754d0ebf8528c63659df03
warisha <warishanaz403@gmail.com>
2024-06-11 19:19:45 -0800

    created number guessing game

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\twelcome to my- Cli Number Guessing Game ");
+
+const randomNumber = Math.floor(Math.random()*5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name : "userGuessedNumber",
+        type : "number",
+        message : "Enter your guess number (Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber){ 
+    console.log("congratulation ! you guess a correct number.");
+}
+else {
+    console.log("sorry , you guess a wrong number");
+}
